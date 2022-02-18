6fc168ab71c71b6e9e323e4e20f83d2e84feed61
Albertoxed <78575415+Albertoxed@users.noreply.github.com>
2022-02-18 11:17:24 -0500

    Add files via upload

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,20 @@
+﻿using ApiJira.Dto;
+using ApiJira.Models;
+using ApiJira.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiJira.Controllers
+{
+    public class EmployeesController : BaseController<Employee, EmployeeDto>
+    {
+        public EmployeesController(IEmployeeService userService) : base(userService)
+        {
+
+        }
+    }
+}
---
+++ Controllers/IssuesController.cs
@@ -0,0 +1,23 @@
+﻿using ApiJira.Dto;
+using ApiJira.Models;
+using ApiJira.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiJira.Controllers
+{
+    [Authorize]
+    public class IssuesController : BaseController<Issue, IssueDto>
+    {
+        public IssuesController(IIssueService issueService) : base(issueService)
+        {
+
+        }
+    }
+}
